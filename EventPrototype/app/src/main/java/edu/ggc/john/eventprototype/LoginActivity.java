5226f6d68ad0db2a0ff1a958b66f009db0901b8d
jtran3 <jtran3@ggc.edu>
2016-10-07 21:19:36 -0400

    setup activity regisiter and login

--- EventPrototype/app/src/main/java/edu/ggc/john/eventprototype/LoginActivity.java
+++ EventPrototype/app/src/main/java/edu/ggc/john/eventprototype/LoginActivity.java
@@ -1,7 +1,12 @@
 package edu.ggc.john.eventprototype;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -9,5 +14,20 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        final EditText eUsername = (EditText) findViewById(R.id.eUsername);
+        final EditText ePassword = (EditText) findViewById(R.id.ePassword);
+        final Button bLogin = (Button) findViewById(R.id.bLogin);
+        final TextView regisiterLink = (TextView) findViewById(R.id.tvRegisiterHere);
+
+        regisiterLink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent regisiterIntent = new Intent(LoginActivity.this, RegisterActivity.class);
+                LoginActivity.this.startActivity(regisiterIntent);
+            }
+        });
     }
+
+
 }
--- EventPrototype/app/src/main/java/edu/ggc/john/eventprototype/RegisterActivity.java
+++ EventPrototype/app/src/main/java/edu/ggc/john/eventprototype/RegisterActivity.java
@@ -2,6 +2,8 @@ package edu.ggc.john.eventprototype;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -9,5 +11,12 @@ public class RegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        final EditText eName = (EditText) findViewById(R.id.eName);
+        final EditText eUsername = (EditText) findViewById(R.id.eUsername);
+        final EditText ePassword = (EditText) findViewById(R.id.ePassword);
+        final EditText eEmail = (EditText) findViewById(R.id.eEmail);
+        final EditText ePhonenumber = (EditText) findViewById(R.id.ePhonenumber);
+        final Button bRegisiter = (Button) findViewById(R.id.bRegisiter);
     }
 }
