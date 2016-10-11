e6c348a8bdee887d8af29e0bf587f0a12f77dd58
jtran3 <jtran3@ggc.edu>
2016-10-11 18:07:13 -0400

    added and woring on login.php.

---
+++ Login.php
@@ -0,0 +1,27 @@
+<?php
+	$con = mysqli_connect("mysql11.000webhost.com", "a1087370_Eventgo", "database1", "a1087370_EventGo");
+	$username = $_POST("username");
+	$password = $_POST("password");
+	
+	$statement = mysql_prepare($con, "SELECT * FROM user WHERE username = ? AND password = ?");
+	mysqli_stmt_bind_param($statement, "ss", $username, $password);
+	mysqli_stmt_execute($statement);
+	
+	mysqli_stmt_store_result($statement);
+	mysqli_stmt_bind_result($statement, $userID, $name, $email, $username, $password, $phonenumber);
+	
+	$response = array();
+	$response["success"] = false;
+	
+	while(mysqli_stmt_fetch($statement))
+	{
+		response["success"] = true;
+		response["name"] = $name;
+		response["email"] = $email;
+		response["password"] = $password;
+		response["username"] = username;
+		response[""]
+	}	
+
+
+?>
