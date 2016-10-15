02b7e8db12b1d89878a7b090c20ac552bcb0c4f5
jtran3 <jtran3@ggc.edu>
2016-10-14 22:54:37 -0400

    added in RegisiterRequest.java and edited RegisterActivity.java

--- EventPrototype/app/src/main/java/edu/ggc/john/eventprototype/RegisterActivity.java
+++ EventPrototype/app/src/main/java/edu/ggc/john/eventprototype/RegisterActivity.java
@@ -1,9 +1,19 @@
 package edu.ggc.john.eventprototype;
 
+import android.app.AlertDialog;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -19,6 +29,49 @@ public class RegisterActivity extends AppCompatActivity {
         final EditText ePhonenumber = (EditText) findViewById(R.id.ePhonenumber);
         final Button bRegisiter = (Button) findViewById(R.id.bRegisiter);
 
+        bRegisiter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final String name = eName.getText().toString();
+                final String username = eUsername.getText().toString();
+                final String password = ePassword.getText().toString();
+                final String email = eEmail.getText().toString();
+                final int phonenumber = Integer.parseInt(ePhonenumber.getText().toString());
+
+                Response.Listener<String> responseListener = new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response)
+                    {
+                        try {
+                            JSONObject jsonResponse = new JSONObject(response);
+                            boolean success = jsonResponse.getBoolean("success");
+                            if(success)
+                            {
+                                Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
+                                RegisterActivity.this.startActivity(intent);
+                            }
+                            else
+                            {
+                                AlertDialog.Builder builder = new AlertDialog.Builder(RegisterActivity.this);
+                                builder.setMessage("Register failed")
+                                        .setNegativeButton("Retry", null)
+                                        .create()
+                                        .show();
+                            }
+
+                        }
+                        catch (JSONException e)
+                        {
+                            e.printStackTrace();
+                        }
+                    }
+                };
+
+                RegisiterRequest regisiterRequest = new RegisiterRequest(name, username, password, email, phonenumber, responseListener);
+                RequestQueue queue = Volley.newRequestQueue(RegisterActivity.this);
+                queue.add(regisiterRequest);
+            }
+        });
 
     }
 }
