32d51e8de72172b4a72ef750875f18cca89d1940
jtran3 <jtran3@ggc.edu>
2016-10-20 20:16:47 -0400

    tried correcting error from the commit didn't work so I reset it.

---
+++ EventPrototype/app/src/main/java/edu/ggc/john/eventprototype/CreateEvent2.java
@@ -0,0 +1,13 @@
+package edu.ggc.john.eventprototype;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CreateEvent2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_event2);
+    }
+}
