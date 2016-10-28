c6d3a7e31360daede25ac5f24f9c4122f518a3a7
jtran3 <jtran3@ggc.edu>
2016-10-27 22:57:12 -0400

    update document on what i did.

---
+++ EventPrototype/app/src/main/java/edu/ggc/john/eventprototype/ManageEvent.java
@@ -0,0 +1,13 @@
+package edu.ggc.john.eventprototype;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ManageEvent extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_manage_event);
+    }
+}
