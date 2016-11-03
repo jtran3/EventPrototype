0bde37131bac3ede6b76a25ee45ea0a9e34038a1
aharri23 <aharri23@ggc.edu>
2016-11-03 18:17:26 -0400

    Added Event list

--- EventPrototype/app/src/main/java/edu/ggc/john/eventprototype/MyEvents.java
+++ EventPrototype/app/src/main/java/edu/ggc/john/eventprototype/MyEvents.java
@@ -1,7 +1,10 @@
 package edu.ggc.john.eventprototype;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MyEvents extends AppCompatActivity
 {
@@ -11,5 +14,20 @@ public class MyEvents extends AppCompatActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_events);
+
+        Button btnShowEvent =(Button)findViewById(R.id.buttonShowEvent);
+
+
+        // Set OnClick Listener on  button  and start AnimalListActivity when clicked on Button
+        btnShowEvent.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+
+                /// Create Intent for AnimalListActivity and Start The Activity
+                Intent intentAnimalList=new Intent(getApplicationContext(),Event_List.class);
+                startActivity(intentAnimalList);
+            }
+        });
     }
 }
