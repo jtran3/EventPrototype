9e8b8d83d004be28aa3326824dcacccc68d87f3c
jtran3 <jtran3@ggc.edu>
2016-11-28 21:44:19 -0500

    I think i finished up the receiver just got to text it in class

--- EventPrototype/app/src/main/java/edu/ggc/john/eventprototype/TextReceiver.java
+++ EventPrototype/app/src/main/java/edu/ggc/john/eventprototype/TextReceiver.java
@@ -5,12 +5,16 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.telephony.SmsMessage;
+import android.util.Log;
+
+import static com.android.volley.VolleyLog.TAG;
 
 /**
  * Created by jtran_000 on 11/27/2016.
  */
 
-public class TextReceiver extends BroadcastReceiver {
+public class TextReceiver extends BroadcastReceiver
+{
 
     public TextReceiver()
     {}
@@ -24,6 +28,26 @@ public class TextReceiver extends BroadcastReceiver {
             SmsMessage[] msgs = null;
 
             String str = "";
-        }
+            if (bundle != null) {
+                // Retrieve the SMS Messages received
+                Object[] pdus = (Object[]) bundle.get("pdus");
+                msgs = new SmsMessage[pdus.length];
 
+                // For every SMS message received
+                for (int i=0; i < msgs.length; i++) {
+                    // Convert Object array
+                    msgs[i] = SmsMessage.createFromPdu((byte[]) pdus[i]);
+                    // Sender's phone number
+                    str += "SMS from " + msgs[i].getOriginatingAddress() + " : ";
+                    // Fetch the text message
+                    str += msgs[i].getMessageBody().toString();
+                    // Newline 🙂
+                    str += "\n";
+                }
+
+                // Display the entire SMS Message
+                Log.d(TAG, str);
+            }
+
+        }
 }
