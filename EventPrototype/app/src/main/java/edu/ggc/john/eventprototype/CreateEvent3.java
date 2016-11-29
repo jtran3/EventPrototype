0102a72cbafaa32c574fa1be1dc455de9d34d180
aharri23 <aharri23@ggc.edu>
2016-11-29 18:28:40 -0500

    almost finished app

--- EventPrototype/app/src/main/java/edu/ggc/john/eventprototype/CreateEvent3.java
+++ EventPrototype/app/src/main/java/edu/ggc/john/eventprototype/CreateEvent3.java
@@ -1,8 +1,16 @@
 package edu.ggc.john.eventprototype;
 
+import android.app.Activity;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.ContentValues;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -14,6 +22,8 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.telephony.SmsManager;
 import android.widget.Toast;
+
+import java.util.ArrayList;
 
 
 public class CreateEvent3 extends AppCompatActivity {
@@ -27,38 +37,33 @@ public class CreateEvent3 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_event3);
         Button yourButton = (Button) findViewById(R.id.button6);
+        eventMessage = (EditText) findViewById(R.id.editText6);
+        message = eventMessage.getText().toString();
 
         yourButton.setOnClickListener(new View.OnClickListener()
         {
             public void onClick(View v)
             {
-                sendSMSMessage();
-                startActivity(new Intent(CreateEvent3.this, MainActivity.class));
+                sendSMS2(v);
+          //      startActivity(new Intent(CreateEvent3.this, MainActivity.class));
             }
         });
     }
 
-    protected void sendSMSMessage()
-    {
-        eventMessage = (EditText) findViewById(R.id.editText6);
+    private void sendSMS(String phoneNumber, String message) {
+        SmsManager sms = SmsManager.getDefault();
+        sms.sendTextMessage(phoneNumber, null, message, null, null);
+    }
 
-        for (int i = 0; i < AndroidListViewActivity.phoneNumbersSelected.size(); i++) {
-            phoneNo = AndroidListViewActivity.phoneNumbersSelected.get(i);
-            message = eventMessage.getText().toString();
-            SmsManager smsManager = SmsManager.getDefault();
-            smsManager.sendTextMessage(phoneNo, null, message, null, null);
 
-            if (ContextCompat.checkSelfPermission(this,
-                    Manifest.permission.SEND_SMS)
-                    != PackageManager.PERMISSION_GRANTED) {
-                if (ActivityCompat.shouldShowRequestPermissionRationale(this,
-                        Manifest.permission.SEND_SMS)) {
-                } else {
-                    ActivityCompat.requestPermissions(this,
-                            new String[]{Manifest.permission.SEND_SMS},
-                            MY_PERMISSIONS_REQUEST_SEND_SMS);
-                }
-            }
+
+    public void sendSMS2(View view) {
+
+        for (int i = 0; i<AndroidListViewActivity.phoneNumbersSelected.size(); i++) {
+            String phoneNumber = AndroidListViewActivity.phoneNumbersSelected.get(i);
+            Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("sms:" + phoneNumber));
+            intent.putExtra("sms_body", message);
+            startActivity(intent);
         }
     }
 
